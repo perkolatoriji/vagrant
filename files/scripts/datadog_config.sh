b6162723cdb4e48f6a9d1953d97fe729b660d2dc
Carlos Ijalba <carlos.ijalba@seera.sa>
2020-10-30 11:52:35 +0100

    fcreated external variables in ansible playbook - datadog

--- files/scripts/datadog_config.sh
+++ files/scripts/datadog_config.sh
@@ -4,6 +4,9 @@
 
 #Main
 
-DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$(cat ~/secrets/datadog_api.sec)  DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+DD=$(grep datadog_api_key /home/vagrant/secrets/datadog_api.sec | cut -f2 -d":")
+
+
+DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$DD  DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 
 #End

c479dc2860c89ca4ce9ec648b257344c37dde182
Carlos Ijalba <carlos.ijalba@seera.sa>
2021-01-13 21:45:42 +0100

    mejoras en project_check.sh

--- project_check.sh
+++ project_check.sh
@@ -1,24 +1,28 @@
 echo "O-> Starting project check:"
-echo o
+echo .
 echo "o-> grafana server up:"
-curl 192.168.11.11:3000
+curl -s 192.168.11.11:3000
 echo "o-- you can access grafana via: http://192.168.11.11:3000"
-echo o
+echo .
 echo "o-> nginx server up:"
-curl 192.168.11.12 | grep "<title>" 
+curl -s 192.168.11.12 | grep "<title>" 
 echo "o-- you can access nginx via: http://192.168.11.12"
-echo o
+echo .
 echo "o-> apache2 server up:"
-curl 192.168.11.13 | grep "<title>"
+curl -s 192.168.11.13 | grep "<title>"
 echo "o-- you can access apache2 via: http://192.168.11.13"
-echo o
+echo .
 echo "o-> prometheus server up:"
-curl 192.168.11.14:9090
+curl -s 192.168.11.14:9090
 echo "o-- you can access prometheus via: http://192.168.11.14:9090/graph"
-echo o
+echo .
 echo "o-> prometheus-pushgateway up:"
-curl 192.168.11.14:9091 | grep  "<title>"
+curl -s 192.168.11.14:9091 | grep  "<title>"
 echo "o-- you can access prometheus-pushgateway via: http://192.168.11.14:9091"
-echo o
+echo .
+echo "o-> prometheus-alertmanager up:"
+curl -s 192.168.11.14:9093 | grep  "<title>"
+echo "o-- you can access prometheus-alertmanager via: http://192.168.11.14:9093"
+echo .
 echo "O-> project check finished."
 
